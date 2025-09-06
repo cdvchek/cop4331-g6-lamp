a279ca59b9a6168187e9d3a46d1e2805fb7ea2b7
Carsdan Dvorachek <cdvchek@gmail.com>
2025-09-06 10:57:23 -0400

    hooking up signup and login frontend to backend

---
+++ public/js/login.js
@@ -0,0 +1,37 @@
+const username_login_el = document.getElementById('username-input');
+const password_login_el = document.getElementById('password-input');
+const login_btn_el = document.getElementById('login-btn');
+
+const login = async (username, password) => {
+    const payload = {
+        login: username,
+        password: password
+    };
+
+    const res = await fetch(base_url + "/API/login.php", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(payload)
+    });
+
+    if (!res.ok) {
+        // TODO: error handling
+    }
+
+    const data = await res.json();
+    if (data.status == "success") {
+        localStorage.setItem("userId", data.id);
+        localStorage.setItem("firstName", data.firstName);
+        localStorage.setItem("lastName", data.lastName);
+
+        window.location.href = base_url + "/view/dashboard.html"
+    } else {
+        // TODO: error handling
+    }
+}
+
+const doLogin = () => {
+    login(username_login_el.value.trim(), password_login_el.value);
+}
+
+login_btn_el.addEventListener('click', doLogin);
---
+++ public/js/signup.js
@@ -0,0 +1,35 @@
+const f_name_el = document.getElementById('first-name-input');
+const l_name_el = document.getElementById('last-name-input');
+const username_el = document.getElementById('username-input');
+const password_el = document.getElementById('password-input');
+const signup_btn_el = document.getElementById('signup-btn');
+
+const signup = async () => {
+    const payload = {
+        fistName: f_name_el.value.trim(),
+        lastName: l_name_el.value.trim(),
+        username: username_el.value.trim(),
+        password_el: password_el.value
+    };
+
+    const res = await fetch(base_url + "/API/register.php", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(payload)
+    });
+
+    if (!res.ok) {
+        // TODO: error handling
+    }
+
+    const data = await res.json();
+    if (data.status == "success") {
+        console.log(data);
+    } else {
+        // TODO: error handling
+    }
+
+    login(username_el.value.trim(), password_el.value);
+}
+
+signup_btn_el.addEventListener('click', signup);
