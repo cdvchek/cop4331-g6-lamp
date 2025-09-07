c85ee84a46fa4d3f9c2628e959c8de99a5016de9
Carsdan Dvorachek <67628009+ChuckDvchek@users.noreply.github.com>
2025-09-07 16:01:12 -0400

    added comments to the login js file

--- public/js/login.js
+++ public/js/login.js
@@ -1,26 +1,34 @@
+// Grab the html elements we want
 const username_login_el = document.getElementById('username-input');
 const password_login_el = document.getElementById('password-input');
 const login_btn_el = document.getElementById('login-btn');
 
+// Function that runs when form is submitted or login button is pushed
 const login = async (e) => {
     e.preventDefault();
     
+    // Package the data to send to the back end
     const payload = {
         login: username_login_el.value.trim(),
         password: password_login_el.value
     };
 
+    // Send the package to the backend and await the response (res)
     const res = await fetch(base_url + "/API/login.php", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(payload)
     });
 
+    // Read the response for error handling
     if (!res.ok) {
         // TODO: error handling
     }
 
+    // Get the data from the response
     const data = await res.json();
+
+    // Save the data locally in the browser
     if (data.status == "success") {
         localStorage.setItem("userId", data.id);
         localStorage.setItem("firstName", data.firstName);
@@ -32,4 +40,5 @@ const login = async (e) => {
     }
 }
 
+// Assign the login button an on "click" event that runs the login function
 login_btn_el.addEventListener('click', login);
