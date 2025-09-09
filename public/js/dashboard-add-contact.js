6d9ab825a1d3179c68aa2c4e6dd4bb80ea024581
Carsdan Dvorachek <cdvchek@gmail.com>
2025-09-09 07:49:17 -0400

    finalized dashboard add contact form visuals

--- public/js/dashboard-add-contact.js
+++ public/js/dashboard-add-contact.js
@@ -1,6 +1,11 @@
 const add_contact_menu_open_el = document.getElementById('add-contact');
 const search_wrapper_el = document.getElementById('search-wrapper');
 const first_name_wrapper = document.getElementById('fn-add-wrapper');
+const last_name_wrapper = document.getElementById('ln-add-wrapper');
+const phone_wrapper = document.getElementById('phone-add-wrapper');
+const email_wrapper = document.getElementById('email-add-wrapper');
+const add_contact_btn = document.getElementById('final-add-contact-btn');
+const contact_list_el = document.getElementById('contact-list');
 
 let add_menu_toggle = false;
 
@@ -10,6 +15,11 @@ const toggle_add_menu = () => {
     const add_btn_classname = add_menu_toggle ? 'add-open' : '';
     add_contact_menu_open_el.setAttribute('class', add_btn_classname);
     first_name_wrapper.setAttribute('class', 'add-contact-wrapper ' + add_btn_classname);
+    last_name_wrapper.setAttribute('class', 'add-contact-wrapper ' + add_btn_classname);
+    phone_wrapper.setAttribute('class', 'add-contact-wrapper ' + add_btn_classname);
+    email_wrapper.setAttribute('class', 'add-contact-wrapper ' + add_btn_classname);
+    add_contact_btn.setAttribute('class', add_btn_classname);
+    contact_list_el.setAttribute('class', add_btn_classname);
 
     const search_wrapper_classname = add_menu_toggle ? 'open' : '';
     search_wrapper_el.setAttribute('class', search_wrapper_classname);
