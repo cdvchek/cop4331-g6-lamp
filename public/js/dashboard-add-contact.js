2245751ed24f5fe807534d90d6f695143d4c8b68
Carsdan Dvorachek <cdvchek@gmail.com>
2025-09-09 08:15:55 -0400

    changed variable name to not collide with other files

--- public/js/dashboard-add-contact.js
+++ public/js/dashboard-add-contact.js
@@ -5,7 +5,7 @@ const last_name_wrapper = document.getElementById('ln-add-wrapper');
 const phone_wrapper = document.getElementById('phone-add-wrapper');
 const email_wrapper = document.getElementById('email-add-wrapper');
 const add_contact_btn = document.getElementById('final-add-contact-btn');
-const contact_list_el = document.getElementById('contact-list');
+const contact_list_el_add = document.getElementById('contact-list');
 
 let add_menu_toggle = false;
 
@@ -19,7 +19,7 @@ const toggle_add_menu = () => {
     phone_wrapper.setAttribute('class', 'add-contact-wrapper ' + add_btn_classname);
     email_wrapper.setAttribute('class', 'add-contact-wrapper ' + add_btn_classname);
     add_contact_btn.setAttribute('class', add_btn_classname);
-    contact_list_el.setAttribute('class', add_btn_classname);
+    contact_list_el_add.setAttribute('class', add_btn_classname);
 
     const search_wrapper_classname = add_menu_toggle ? 'open' : '';
     search_wrapper_el.setAttribute('class', search_wrapper_classname);
