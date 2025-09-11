b8cd3043c2c27c7823f42d1932ad68765c0b8d94
Carsdan Dvorachek <cdvchek@gmail.com>
2025-09-10 23:21:41 -0400

    making things simpler later

--- public/js/dashboard-search.js
+++ public/js/dashboard-search.js
@@ -3,6 +3,8 @@ const contact_list_el = document.getElementById('contact-list');
 const create_contact_el = (first_name, last_name, email, phone) => {
     const contact_li = document.createElement('li');
     contact_li.setAttribute('class', 'contact-entry');
+    contact_li.setAttribute('data-fname', first_name);
+    contact_li.setAttribute('data-lname', last_name);
     contact_li.setAttribute('data-email', email);
     contact_li.setAttribute('data-phone', phone);
 
