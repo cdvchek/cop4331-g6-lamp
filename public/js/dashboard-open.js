698dc26cf2f2e312af3294e93050d96f12bc5b76
Carsdan Dvorachek <cdvchek@gmail.com>
2025-09-10 22:17:08 -0400

    moved background to images instead of icon and added login animation

---
+++ public/js/dashboard-open.js
@@ -0,0 +1,7 @@
+const opener_el = document.getElementById('opener');
+
+opener_el.setAttribute('class', 'end');
+
+const body_open_el = document.getElementById('body');
+
+body_open_el.setAttribute('class', 'end');
