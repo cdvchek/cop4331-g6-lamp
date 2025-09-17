e6966a3dc6d57dea9551d56367a5f4dc704fd76b
Carsdan Dvorachek <cdvchek@gmail.com>
2025-09-17 13:47:40 -0400

    fixed remember me typo

--- public/js/login/login-remember-me.js
+++ public/js/login/login-remember-me.js
@@ -1,14 +1,10 @@
 // Uses element references from login-callback
-const check_remember_me = () => {
-    console.log("running");
-    
+const check_remember_me = () => {    
     const username = localStorage.getItem('login-username');
     const password = localStorage.getItem('login-password');
-
-    console.log(username, password);
     
     if (username && password) {
-        remember_me.checked = true;
+        remember_me_el.checked = true;
         username_login_el.value = username;
         password_login_el.value = password;
     }
