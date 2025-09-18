a54c3601f03e2e12a6271c710ed995e528f57344
Kathlea Corla <kathleacorla@yahoo.com>
2025-09-18 11:10:55 -0400

    ADDED: session.php -- Checks to make sure user is logged in or not for back end Checks -- Note: make sure to log in first then check. It should also work if     you do the logout.php and will return the 401 error.

---
+++ public/API/session.php
@@ -0,0 +1,29 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+session_start();
+
+// Function to respond with Json and exit
+function respond($statusCode, $arr){
+    http_response_code($statusCode);
+    echo json_encode($arr);
+    exit();
+}
+
+// Check if user session exists
+if(isset($_SESSION['user_id'])){
+    // If Session Exists --> logged in
+    respond(200, [
+        "status" => "success",
+        "message" => "User is logged in",
+        "UserID" => $_SESSION["user_id"],
+        "firstName" => $_SESSION['first_name'] ?? null,
+        "lastName" => $_SESSION['last_name'] ?? null
+    ]);
+}
+else{
+    // There Is NO Session -> not authorized
+    respond(401, [
+        'status'=> 'error',
+        'message'=> 'Unauthorized, no active session'
+    ]);
+}
