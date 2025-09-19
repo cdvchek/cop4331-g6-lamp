b19a0e7c52af3ea3e03dcd2bcdaa233e9883cc0d
Carsdan Dvorachek <cdvchek@gmail.com>
2025-09-19 12:41:51 -0400

    cancel/save buttons only show up on edit mode and they are wired up to toggle edit mode. Also slight refactoring of toggle_edit_mode for more readability

--- public/js/dashboard/dashboard-edit-contact.js
+++ public/js/dashboard/dashboard-edit-contact.js
@@ -6,14 +6,7 @@ const lname_input_el = document.getElementById('dashboard-lname-input-edit');
 const email_input_el = document.getElementById('dashboard-email-input-edit');
 const phone_input_el = document.getElementById('dashboard-phone-input-edit');
 
-const clear_edit_inputs = () => {
-    fname_input_el.value = "";
-    lname_input_el.value = "";
-    email_input_el.value = "";
-    phone_input_el.value = "";
-}
-
-const switch_span_inputs = (edit_mode) => {
+const toggle_span_inputs = (edit_mode) => {
     const span_display = edit_mode ? "none" : "block";
     const input_display = edit_mode ? "block" : "none";
     
@@ -28,11 +21,28 @@ const switch_span_inputs = (edit_mode) => {
     phone_input_el.style.display = input_display;
 }
 
-const set_edit_inputs = () => {
-    fname_input_el.value = big_contact_fname.textContent;
-    lname_input_el.value = big_contact_lname.textContent;
-    email_input_el.value = big_contact_email.textContent;
-    phone_input_el.value = big_contact_phone.textContent;
+const toggle_edit_inputs = (edit_mode) => {
+    if (edit_mode) {
+        fname_input_el.value = big_contact_fname.textContent;
+        lname_input_el.value = big_contact_lname.textContent;
+        email_input_el.value = big_contact_email.textContent;
+        phone_input_el.value = big_contact_phone.textContent;
+    } else {
+        fname_input_el.value = "";
+        lname_input_el.value = "";
+        email_input_el.value = "";
+        phone_input_el.value = "";
+    }
+}
+
+const toggle_cancel_save_btns = (edit_mode) => {
+    if (edit_mode) {
+        save_btn_el.classList.add("edit");
+        cancel_btn_el.classList.add("edit");
+    } else {
+        save_btn_el.classList.remove("edit");
+        cancel_btn_el.classList.remove("edit");
+    }
 }
 
 let edit_mode = false;
@@ -41,21 +51,20 @@ const toggle_edit_mode = (e) => {
     while (target.getAttribute('data-edit') !== 'true') target = target.parentNode;
     const button = target.getAttribute('data-edit-source');
 
-    if (button === "menu-edit" && edit_mode) return toggle_menu();
-    if (button === "menu-edit" && selected_contact_id === "") return toggle_menu();
+    if (button === "menu-edit") {
+        toggle_menu();
+        if (edit_mode || selected_contact_id === "") return;
+    }
     
     edit_mode = (button === "logout") ? false : !edit_mode;
 
-    clear_edit_inputs();
-    switch_span_inputs(edit_mode);
-
-    if (edit_mode) {
-        set_edit_inputs();
-        toggle_menu();
-    }
+    toggle_edit_inputs(edit_mode);
+    toggle_span_inputs(edit_mode);
+    toggle_cancel_save_btns(edit_mode);
 }
 
 edit_btn_el.addEventListener('click', toggle_edit_mode);
+cancel_btn_el.addEventListener('click', toggle_edit_mode);
 
 const sync_contact_icon_letter = (e) => {
     const value = e.target.value;
@@ -67,6 +76,9 @@ const sync_contact_icon_letter = (e) => {
 
 fname_input_el.addEventListener('input', sync_contact_icon_letter);
 
-const save_contact = async () => {
+const save_contact = async (e) => {
 
+    toggle_edit_mode(e);
 }
+
+save_btn_el.addEventListener('click', save_contact);
